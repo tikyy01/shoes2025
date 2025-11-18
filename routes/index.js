17067e8b30e306c18e0187142c43800bcf7a7b5f
tikyydc <ervinaksizov@gmail.com>
2025-11-18 15:41:45 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Jordan 1 low Travis Scott*/
+router.get('/jordan1', function(req, res, next) {
+    res.send("<h1>Страница Jordan 1 low Travis Scott</h1>")
+});
+
+/* Страница Air Force 1 low */
+router.get('/force1', function(req, res, next) {
+    res.send("<h1>Страница Air Force 1 low</h1>")
+});
+
+/* Страница Air Force 1 Off White */
+router.get('/offwhite', function(req, res, next) {
+    res.send("<h1>Страница Air Force 1 Off White</h1>")
+});
+
 module.exports = router;
