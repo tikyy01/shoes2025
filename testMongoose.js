2fa7a250ef7c42e65e00af0996ebebb297c76a1c
tikyydc <ervinaksizov@gmail.com>
2025-12-01 01:40:31 +0300

    shoes_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
+const sneakerSchema = new mongoose.Schema({ 
+  name: String 
+});
 
-const sneaker = mongoose.model('Shoes', { name: String });
+sneakerSchema.methods.show = function () {
+  console.log(`${this.name} — в магазине прямо сейчас!`);
+};
 
+const Sneaker = mongoose.model('Shoes', sneakerSchema);
 
-const airForce = new sneaker({ name: 'air force' });
-airForce.save().then(() => console.log('Кроссовок успешно сохранён!)'));
+const airForce = new Sneaker({ name: 'air force' });
+
+airForce.save().then(() => {
+  console.log('Кроссовок успешно сохранён!');
+  airForce.show();  // ← вот здесь сработает наш метод
+});
