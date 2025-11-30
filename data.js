3db33ce6a332a195af9c4b60710ed42d057fc890
tikyydc <ervinaksizov@gmail.com>
2025-12-01 01:02:53 +0300

    shoes_6_2

---
+++ data.js
@@ -0,0 +1,22 @@
+var data = [{
+   title: 'Jordan 1 low Travis Scott',
+   nick: 'Jordan 1 low Travis Scott',
+   avatar: '/images/jordan1.jpg',
+   desc: 'Культовая баскетбольная модель Nike, выпущенная в 1985 году. Легендарный силуэт, популярный в уличной моде и кроссовочной культуре.'
+},
+{
+   title: 'Air Force 1 low',
+   nick: 'Air Force 1 low',
+   avatar: '/images/airforce.jpg',
+   desc: 'Легендарные кроссовки 1982 года. Первые Nike с технологией Air. Белая расцветка White on White стала иконой уличного стиля и хип-хоп культуры.'
+},
+{
+   title: 'Air Force 1 Off White',
+   nick: 'Air Force 1 Off White',
+   avatar: '/images/offwhite.jpg',
+   desc: 'Коллаборация Nike и Вирджила Абло. Деконструкция, фирменные кавычки, яркие бирки. Икона уличной моды и коллекционная ценность.'
+}
+];
+
+
+module.exports.data = data;
