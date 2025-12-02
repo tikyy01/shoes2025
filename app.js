bf0bb0c2b9e157385ca7992cb35330da5c7e9cd3
tikyydc <ervinaksizov@gmail.com>
2025-12-02 22:19:59 +0300

    shoes_9_1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/shoes2025')
+var session = require("express-session")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var indexShoes = require('./routes/shoes');
@@ -21,6 +22,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+ secret: "ThreeShoes",
+ cookie:{maxAge:60*1000},
+ proxy: true,
+ resave: true,
+ saveUninitialized: true
+}))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
