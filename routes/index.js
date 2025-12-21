de4f42c0a960aa4746adc76b411b188e651f0dc4
tikyydc <ervinaksizov@gmail.com>
2025-12-22 00:31:09 +0300

    shoes_10_1

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express',  counter:req.session.counter });
 });
 
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+ res.render('logreg',{title: 'Вход'});
+ });
 
 
 
