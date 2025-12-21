9ad44e3aa02257737e8c3edad005effb6d094184
tikyydc <ervinaksizov@gmail.com>
2025-12-21 16:07:11 +0300

    shoes_9_4

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.use(session({
 }))
 
 
+
+app.use(function(req,res,next){
+ req.session.counter = req.session.counter + 1 || 1
+ next()
+ })
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Shoes', indexShoes);
