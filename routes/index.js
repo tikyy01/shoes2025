b867104a0d9362ba5296987a4dccee3292062831
tikyydc <ervinaksizov@gmail.com>
2025-12-22 00:45:41 +0300

    shoes_10_3

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,14 @@ router.get('/', function(req, res, next) {
 router.get('/logreg', function(req, res, next) {
  res.render('logreg',{title: 'Вход'});
  });
+/* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  console.log(username);
+  console.log(password);
 
+});
 
 
 module.exports = router;
